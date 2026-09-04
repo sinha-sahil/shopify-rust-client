8d2d55badc500ca990726616d30ed5a7e324deb1
Anirudh Naruka <anirudh.naruka@Anirudh-Naruka-KVM969LFWH.local>
2026-09-04 16:05:32 +0530

    AC-360: feat: removed variant location access from query

--- src/admin/bulk_operation/remote.rs
+++ src/admin/bulk_operation/remote.rs
@@ -671,10 +671,6 @@ pub fn inventory_items_query(filter: Option<&str>) -> String {
                 name
                 quantity
               }
-              location {
-                id
-                name
-              }
               updatedAt
             }
           }
